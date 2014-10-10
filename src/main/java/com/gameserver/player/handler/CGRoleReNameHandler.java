e2ff80400f56856941e73759769a091c7cba5000
Administrator <Administrator@TAISHIJI>
2014-10-10 18:59:01 +0800

    添加方法

---
+++ src/main/java/com/gameserver/player/handler/CGRoleReNameHandler.java
@@ -0,0 +1,48 @@
+package com.gameserver.player.handler;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.common.msg.BaseBean;
+import com.common.msg.BaseBean.BaseMessage;
+import com.common.msg.PlayerBean.CGRoleReName;
+import com.common.msg.PlayerBean.GCGetRoleList;
+import com.common.msg.PlayerBean.GCRoleReName;
+import com.gameserver.common.globals.server.impl.ServerManager;
+import com.gameserver.common.handler.IMessageHandler;
+import com.gameserver.human.Human;
+import com.gameserver.player.Player;
+import com.gameserver.player.PlayerState;
+/**
+ * 用户重命名消息信息处理器
+ * @author Administrator
+ *
+ */
+@Scope("prototype")
+@Component
+public class CGRoleReNameHandler implements IMessageHandler{
+	private CGRoleReName cgRoleReName;
+	private Player player;
+	@Override
+	public void execute() {
+		Human human=player.getHuman();
+		if(human==null) return;
+		long roleId=cgRoleReName.getRoleId();
+		String roleName=cgRoleReName.getRoleName();
+		human.setName(roleName);
+		BaseMessage.Builder myMessage=BaseMessage.newBuilder();
+		GCRoleReName.Builder gcRoleReName=GCRoleReName.newBuilder();
+		gcRoleReName.setRoleName(roleName);
+		myMessage.setType(BaseMessage.Type.PLAYERMESSAGE);
+		myMessage.setMessageCode(BaseMessage.MessageCode.GCROLERENAME);
+		myMessage.setExtension(BaseBean.gcRoleReName, gcRoleReName.build());
+		player.sendMessage(myMessage.build());
+	}
+
+	@Override
+	public void setMessage(BaseMessage baseMessage, Player player) {
+		this.cgRoleReName=baseMessage.getExtension(BaseBean.cgRoleReName);
+		this.player=player;
+	}
+
+}
